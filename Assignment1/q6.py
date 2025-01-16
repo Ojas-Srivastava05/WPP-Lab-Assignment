962db66464a31d9bb9cd0e4c79a0b008d0052d07
Ojas-Srivastava05 <srivastavaojas454@gmail.com>
2025-01-16 17:38:22 +0530

    Update q6.py

--- Assignment1/q6.py
+++ Assignment1/q6.py
@@ -1,16 +1,33 @@
-pointx=[]#list to store the x coordinates of the points
-pointy=[]#list to store the y coordinates of the points
-pointz=[]#list to store the z coordinates of the points
-points=[pointx,pointy,pointz]#making a list to store the points
+pointx = []
+pointy = []
+pointz = []
+points = [pointx, pointy, pointz]
+
 for i in range(10):
-    x=int(input("ENTER THE X COORDINATE OF THE POINT"))
-    y=int(input("ENTER THE Y COORDINATE OF THE POINT"))
-    z=int(input("ENTER THE Z COORDINATE OF THE POINT"))
+    x = int(input("ENTER THE X COORDINATE OF THE POINT: "))
+    y = int(input("ENTER THE Y COORDINATE OF THE POINT: "))
+    z = int(input("ENTER THE Z COORDINATE OF THE POINT: "))
     pointx.append(x)
     pointy.append(y)
     pointz.append(z)
-    points=[pointx,pointy,pointz]
-    print(points)
-   
-    
+
+shortest_distance = float('inf')
+shortest_points = (None, None)
+
+for i in range(10):
+    for j in range(i + 1, 10):
+        distance = ((points[0][i] - points[0][j]) ** 2 +
+                     (points[1][i] - points[1][j]) ** 2 +
+                     (points[2][i] - points[2][j]) ** 2) ** 0.5
+        
+        if distance < shortest_distance:
+            shortest_distance = distance
+            shortest_points = (i, j)
+
+if shortest_points[0] is not None:
+    print(f"The shortest distance is {shortest_distance:.2f} between points {shortest_points[0]} and {shortest_points[1]}")
+else:
+    print("No points were provided.")
+
+
 
