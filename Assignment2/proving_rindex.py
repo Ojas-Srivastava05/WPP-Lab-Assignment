c4a61bfdda414c98f96241f40a611d2fc73852c4
Ojas-Srivastava05 <srivastavaojas454@gmail.com>
2025-01-23 15:40:48 +0530

    Create proving_rindex.py

---
+++ Assignment2/proving_rindex.py
@@ -0,0 +1,16 @@
+def indexfunction(sentence,letter):
+    new_word=""
+    value=0
+    for i in range(len(sentence),len(letter)-1,-1):
+        for k in range(len(letter)):
+            
+            
+            
+
+    
+
+def main():
+    string=input("Enter the main string ")
+    sub_string=input("Enter the substring")
+    indexfunction(string,sub_string)
+
