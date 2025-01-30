604582c7c133e9650f9e1fb0fc3aa1141e942ebd
Ojas-Srivastava05 <srivastavaojas454@gmail.com>
2025-01-30 14:34:10 +0530

    .

---
+++ Assignment2/replace.py
@@ -0,0 +1,26 @@
+a=input("enter string:")
+b=[input("enter part of string to replace:"),input("enter replaced part:")]
+if len(b[0])==len(b[1]):
+    c=b[0]
+    d=b[1]
+    z=''
+    for i in a:
+        for j in range(len(c)):
+            if i==b[j]:
+                z=z+d[j]
+            else:
+                z=z+i
+    print("replaced string is:",z)
+else:
+    c=b[0]
+    d=b[1]
+    z=''
+    for i in a:
+        for j in range(len(c)):
+            if i==b[j]:
+                for k in range(len(d)):
+                    z=z+d[k]
+            else:
+                z=z+i
+    print("replaced string is:",z)
+    
