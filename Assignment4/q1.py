2366415d3b7a5930a7623a401a35685284815ddc
Ojas-Srivastava05 <srivastavaojas454@gmail.com>
2025-02-06 16:28:03 +0530

    Update q1.py

--- Assignment4/q1.py
+++ Assignment4/q1.py
@@ -4,19 +4,6 @@ def palindromemaker(sentence):
     n=len(sentence)
     for i in range(n // 2):
         count += abs(ord(sentence[i]) - ord(sentence[n - i - 1]))
-    # print(len(sentence))
-    # if len(sentence)%2==1:
-    #     for i in range (0,(len(sentence)//2)):
-    #         temp1.append(sentence[i])
-    #         temp2.append(sentence[i+(len(sentence)//2)])
-    #         count=count+abs((ord(temp2[i])-ord(temp1[i])))
-    # elif len(sentence)%2==0:
-    #     for i in range (0,(len(sentence)//2)):
-    #         temp1.append(sentence[i])
-    #         temp2.append(sentence[i+(len(sentence)//2)])
-    #         count=count+abs((ord(temp2[i])-ord(temp1[i])))
-    # print(temp1)
-    # print(temp2)
         
     return count
 
