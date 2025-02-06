1bbb299a74fca30dc3f282657a17f2b9cc90133b
Ojas-Srivastava05 <srivastavaojas454@gmail.com>
2025-02-06 15:45:55 +0530

    Update q1.py

--- Assignment4/q1.py
+++ Assignment4/q1.py
@@ -1,8 +1,21 @@
 #kisi bhi string ko palindrome banane mei minimum number of steps batane hain
 def palindromemaker(sentence):
-    sentence=sentence.split()
     
-    pass
+    temp1=[]
+    temp2=[]
+    print(len(sentence))
+    if len(sentence)%2==1:
+        for i in range (0,(len(sentence)//2)):
+            temp1.append(sentence[i])
+            temp2.append(sentence[i+(len(sentence)//2)])
+    elif len(sentence)%2==0:
+        for i in range (0,(len(sentence)//2)):
+            temp1.append(sentence[i])
+            temp2.append(sentence[i+(len(sentence)//2)])
+    print(temp1)
+    print(temp2)
+        
+    return 0
 
 def main():
     test=int(input("Enter the number of test cases"))
@@ -12,3 +25,6 @@ def main():
           result[i]=palindromemaker(sentence)
     
     print(sentence)
+    
+if __name__=='__main__':
+    main()
