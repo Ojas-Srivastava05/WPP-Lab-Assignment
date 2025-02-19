1184e7e92977d0611df70f5abc4d2fa74e7014f9
Ojas Srivastava <srivastavaojas454@gmail.com>
2025-02-19 21:56:30 +0530

    Update q9.py

--- Assignment7and8/q9.py
+++ Assignment7and8/q9.py
@@ -7,7 +7,7 @@ def tokenize(text):
     emails = re.findall(r'[\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,}', text)
     dates = re.findall(r'\b\d{1,2}[-/\.]\d{1,2}[-/\.]\d{2,4}\b', text)
     numbers = re.findall(r'\b\d+[.,]?\d*\b', text)
-    punctuation = re.findall(r'[.,!?;:"'()\[\]{}]', text)
+    punctuation = re.findall(r'[.,!?;:\\"\'()\[\]{}]', text)
     words = re.findall(r'\b\w+\b', text)
     
     for url in urls:
