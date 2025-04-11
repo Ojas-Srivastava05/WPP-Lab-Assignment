2305f1552bef3db8fd04c9c77517c3285c1c14f3
Ojas Srivastava <srivastavaojas454@gmail.com>
2025-04-11 10:18:08 +0530

    .

--- Assignment10/Q1.py
+++ Assignment10/Q1.py
@@ -1,22 +1,15 @@
 #Queen's problem
 import random
-def Queen(x,y):
-    count=0
-    if count==0:
-        x=0
-        y=random.randint(0,7)
-        count+=1
-    print(x)
-    print(y)
-    invalid=[[x+1,y+1],[x+1,y],[x+1,y-1],[x-1,y+1],[x-1,y],[x-1,y+1]]
-    for i in range(0,x+1):
-        for j in range(0,y+1):
+import numpy as np
+x=random.randint(0,8)
+y=random.randint(0,8)
+matrix=np.arrange(1,65)
+matrix=np.reshape()
+print(matrix)
             
             
             
             
             
-    
-if __name__ == "__main__":
-    Queen(0,0)
+            
 
